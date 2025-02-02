4526954b63e79938f2727d8a604361739e942c5c
gitmohamedhamoudahub <eng.mohamed.hobby@gmail.com>
2025-02-02 07:39:13 -0500

    Adding navBar bar

---
+++ server.js
@@ -0,0 +1,52 @@
+// DEPENDENCIES
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose'); // require mongoose package
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+require('dotenv').config();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// const Book = require("./models/book.js"); // Import the Book Model
+
+// MIDDLEWARE
+// Connect mongodb using connection string in the .env file
+// Console log connection status at start
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}!`)
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); // method override - used to aid in the functionality of a delete.
+app.use(morgan("dev")); // morgan logs status of processed requests in our console.
+
+
+// ROUTES
+
+// HOME / LANDING PAGE - Renders a welcome page in the browser
+app.get("/", async (req, res) => {
+    res.render("home.ejs")
+});
+
+// INDEX BOOK ROUTE - Renders a page with a list of our book collection
+
+// NEW BOOK ROUTE - Renders form for user to enter new book data.
+
+// DELETE - Removes a book from the db.
+
+// UPDATE - Action that changes previously posted data
+
+// CREATE - Action to create new book entry
+
+// EDIT - Renders form to edit existing book data
+
+// SHOW - Route to render page that shows individual book data.
+
+// PORT STUFF!
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log("Server Listening On Port: ", PORT);
+});
